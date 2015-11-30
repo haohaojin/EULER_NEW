2c171d1af394ceb2dda742de065befcd045e3be1
haohaojin <hao.jin@outlook.com>
2015-11-30 13:32:21 +0800

    test from office

--- Problem 4.py
+++ Problem 4.py
@@ -12,25 +12,26 @@ for k in all:
     second_half = ''
     reversed_second_half = ''
     nlen = 0
-    #print(k)
     nlen = len(str(k))
-    #print(str(nlen))
+
     if nlen % 2 == 1:
         for f in range(0,int((nlen-1)/2)):
             first_half += str(k)[f]
-        #print(first_half)
-
         for s in range(int((nlen+1)/2),nlen):
             second_half += str(k)[s]
-        #print(second_half)
-
         for t in range(0,int((nlen-1)/2)):
             reversed_second_half += second_half[int((nlen-1)/2) - t - 1]
-        #print(reversed_second_half)
-
         if first_half == reversed_second_half:
             all_result.append(k)
-    #else:
+    else:
+        for f in range(0,int(nlen/2)):
+            first_half += str(k)[f]
+        for s in range(int(nlen/2),nlen):
+            second_half += str(k)[s]
+        for t in range(0,int(nlen/2)):
+            reversed_second_half += second_half[int(nlen/2) - t - 1]
+        if first_half == reversed_second_half:
+            all_result.append(k)
 
-print(all_result)
+print(max(all_result))
 print("--- %s seconds ---" % (time.time() - start_time))
